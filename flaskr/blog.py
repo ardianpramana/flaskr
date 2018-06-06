caf27df05f004b2163800937b0c5d24d9765adb3
Ardian Pramana <pramana.ardian@gmail.com>
2018-06-06 11:32:57 +0700

    [add] Add blog post detail to see specific post.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -99,3 +99,9 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+
+@bp.route('/<int:id>/post')
+def post_detail(id):
+    post = get_post(id)
+    return render_template('blog/post_detail.html', post=post)
