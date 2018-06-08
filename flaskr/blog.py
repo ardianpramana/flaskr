42e52e6a31579a9edeaf880de9f80f2885695958
Ardian Pramana <pramana.ardian@gmail.com>
2018-06-08 11:09:51 +0700

    [add] add breadcrumbs on detail page.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -9,9 +9,10 @@ from flaskr.db import get_db
 bp = Blueprint('blog', __name__)
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
     db = get_db()
+
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
@@ -37,8 +38,6 @@ def index():
         'likes_list': likes_list,
         'comments_list': comments_list,
     }
-
-    print(data['comments_list'])
 
     return render_template('blog/index.html', posts=posts, data=data)
 
